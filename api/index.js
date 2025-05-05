5aa8cc9de619db5dec44e1571f493a311eddbd8d
Désirée Vitória <dsrvitoria@gmail.com>
2025-05-05 14:22:54 -0300

    Corrigindo erro

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import livrosRoutes from "./livros.routes.js";
+import livrosRoutes from './routes/livros.routes.js';
 import cors from "cors";
 
 const app = express();
