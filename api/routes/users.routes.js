d60404fe987fbe0377445e276b72c794d74afa7e
odilonskt <odilon123c@gmail.com>
2025-05-06 14:09:41 -0300

    fix: corrigindo erro de sintaxer

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUser, deleteUser, getUsers, updateUser } from "../controllers/user.js";
+import { addUser, deleteUser, getUsers, updateUser } from "../controllers/user.Controller.js";
 
 const router = express.Router()
 
