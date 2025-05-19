bc4228a992b3c04ad9e483c80e4f5084fdfc9b9d
Xima <igorrx24072004@gmail.com>
2025-05-19 16:01:36 -0300

    Creating UserService

---
+++ src/service/user.service.js
@@ -0,0 +1,68 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getAllUsersService() {
+  return prisma.usuario.findMany({
+    select: {
+      id: true,
+      nome: true,
+      email: true,
+      tipo: true,
+      criadoEm: true,
+      telefone: true,
+      ativo: true,
+      fotoUrl: true
+    }
+  });
+}
+
+export async function getUserByIdService(id) {
+  return prisma.usuario.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      nome: true,
+      email: true,
+      tipo: true,
+      criadoEm: true,
+      telefone: true,
+      ativo: true,
+      fotoUrl: true,
+      doacoes: true,
+      campanhas: true
+    }
+  });
+}
+
+export async function createUserService(data) {
+  return prisma.usuario.create({
+    data: {
+      nome: data.nome,
+      email: data.email,
+      senha: data.senha,
+      tipo: data.tipo || 'DONATARIO', // String direta ao invés do enum
+      telefone: data.telefone,
+      fotoUrl: data.fotoUrl
+    }
+  });
+}
+
+export async function updateUserService(id, data) {
+  return prisma.usuario.update({
+    where: { id },
+    data
+  });
+}
+
+export async function deleteUserService(id) {
+  return prisma.usuario.delete({
+    where: { id }
+  });
+}
+
+export async function getUserByEmailService(email) {
+  return prisma.usuario.findUnique({
+    where: { email }
+  });
+}
